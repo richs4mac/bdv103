a26abd00253c3b5c559256c12baea522abe1907c
richs4mac <210944006+richs4mac@users.noreply.github.com>
2025-05-18 01:16:32 +0000

    add filter

--- adapter/assignment-1.ts
+++ adapter/assignment-1.ts
@@ -10,17 +10,16 @@ export interface Book {
 
 // If you have multiple filters, a book matching any of them is a match.
 async function listBooks(filters?: Array<{ from?: number, to?: number; }>): Promise<Book[]> {
-    // if (!filters || filters.length === 0) {
-    //     return books; // No filters, return all books
-    // }
-    // console.log("running listBooks")
-    // return books.filter(book =>
-    //     filters.some(filter =>
-    //         (filter.from === undefined || book.price >= filter.from) &&
-    //         (filter.to === undefined || book.price <= filter.to)
-    //     )
-    // );
-    throw new Error("Todo");
+    if (!filters || filters.length === 0) {
+        return books; // No filters, return all books
+    }
+    console.log("running listBooks");
+    return books.filter(book =>
+        filters.some(filter =>
+            (filter.from === undefined || book.price >= filter.from) &&
+            (filter.to === undefined || book.price <= filter.to)
+        )
+    );
 }
 
 const assignment = "assignment-1";
